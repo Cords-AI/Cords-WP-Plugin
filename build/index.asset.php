46d597818a2ca22a8a64b9ebadc1aa1b631af60e
Billy Hawkes <billyhawkes02@gmail.com>
2025-12-15 11:48:56 -0500

    task: build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-element'), 'version' => 'aa52832a96ea08cb7e49');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-element'), 'version' => '709a8d46a799e01b3cdc');
